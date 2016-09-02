45106892ab0d62b5364b63b70d87ac4bc6fa962f
Rafael Teixeira <rafael.szt@gmail.com>
2016-09-01 21:19:25 -0300

    Add files via upload
    Adicionado conversão das instruções do código fonte para binário.

--- parser/parser.cpp
+++ parser/parser.cpp
@@ -24,29 +24,108 @@ Parser *Parser::get_instancia()
     return instancia;
 }
 
-
 Parser::Parser()
 {
     load_instructions();
     load_operators();
 }
 
-Parser::Parser(const Parser& orig)
-{
-    
-}
-
 Parser::~Parser()
 {
 }
 
-std::string Parser::make_object(std::ifstream &source_file)
+void Parser::load_instructions()
 {
-    std::cout << std::endl << "Checking for sintax errors:" << std::endl;
-    check_sintax(source_file);
-    std::cout << "Ok" << std::endl;
-    return "";
+	std::ifstream symbols_file;
+	symbols_file.open("./data/symbols.d");
+
+	if (!symbols_file.is_open())
+		throw std::runtime_error("Failed to open symbols file.");
+
+	std::cout << "Loading symbols... ";
+	std::string line;
+	for (std::getline(symbols_file, line); !symbols_file.eof(); std::getline(symbols_file, line))
+	{
+		auto line_tokens = split_line(line, ' ');
+		this->instructions.push_back(std::make_pair(line_tokens[0], std::make_pair(line_tokens[1], std::stoi(line_tokens[2]))));
+	}
+	symbols_file.close();
+	std::cout << "Done." << std::endl;
 }
+
+void Parser::load_operators()
+{
+	std::ifstream op_file;
+	op_file.open("./data/operator.d");
+
+	if (!op_file.is_open())
+		throw std::runtime_error("Failed to open operator file.");
+
+	std::cout << "Loading operators... ";
+	std::string line;
+	for (std::getline(op_file, line); !op_file.eof(); std::getline(op_file, line))
+	{
+		auto line_tokens = split_line(line, ' ');
+		this->operators.push_back(std::make_pair(line_tokens[0], line_tokens[1]));
+	}
+	op_file.close();
+	std::cout << "Done." << std::endl;
+}
+
+std::vector<std::string> Parser::split_line(const std::string &text, char sep) {
+    std::vector<std::string> tokens;
+    std::size_t start = 0, end = 0;
+    while ((end = text.find(sep, start)) != std::string::npos) {
+        std::string temp = text.substr(start, end - start);
+        if (temp != "") tokens.push_back(temp);
+        start = end + 1;
+    }
+    std::string temp = text.substr(start);
+    if (temp != "") tokens.push_back(temp);
+    return tokens;
+}
+
+std::string Parser::dec_to_bin(int dec_num)
+{
+	return std::bitset<8>(dec_num).to_string();
+}
+
+std::string Parser::get_instruction_opcode(std::string op)
+{
+	std::string op_bin;
+	
+	// Verifica tabela de instrucoes
+	for (auto it = this->instructions.begin(); it != this->instructions.end(); it++)
+		if (it->first == op)
+			return it->second.first;
+
+	// Verifica tabela de registradores
+	for (auto it = this->operators.begin(); it != this->operators.end(); it++)
+		if (it->first == op)
+			return it->second;
+	try
+	{
+		return this->dec_to_bin(std::stoi(op));
+	}
+	catch (const std::invalid_argument)
+	{
+		if (!op.empty())
+			return this->dec_to_bin(std::stoi(this->get_symbol_opcode(op)));
+		else
+			return "";
+	}
+
+	return std::string();
+}
+
+std::string Parser::get_symbol_opcode(std::string op)
+{
+	for (auto it = this->symbol_table.begin(); it != this->symbol_table.end(); it++)
+		if (it->first == op)
+			return std::to_string(it->second);
+	return std::string();
+}
+
 bool Parser::is_operator(std::string op)
 {
     for (auto it = this->operators.begin(); it != this->operators.end(); it++)
@@ -70,7 +149,16 @@ void Parser::add_instruction(std::string inst, std::string op1, std::string op2,
 
 void Parser::add_symbol(std::string si, int val)
 {
-    this->symbol_table.push_back(std::make_pair(si, val));
+	// Verifica se o elemento ja existe, se existir e o endereco for invalido substitui
+	for (auto it = this->symbol_table.begin(); it != this->symbol_table.end(); it++)
+		if (si == it->first)
+			if (it->second == -1)
+			{
+				it->second = val;
+				return;
+			}
+				
+	this->symbol_table.push_back(std::make_pair(si, val));
 }
 
 bool Parser::check_sintax(std::ifstream &source_file)
@@ -137,6 +225,18 @@ bool Parser::check_sintax(std::ifstream &source_file)
                             auto error = "Error in line " + std::to_string(line_count) +  ". Not an operator.";
                             throw std::runtime_error(error);
                         }
+						else
+						{
+							// Tenta converter para inteiro
+							try
+							{
+								std::stoi(operand_token[1]);
+							}
+							catch (const std::invalid_argument)
+							{
+								this->add_symbol(operand_token[1], -1);
+							}
+						}
                     }
                     this->add_instruction(instruction_token[0], operand_token[0], operand_token[1], line_count);
                 }
@@ -156,7 +256,7 @@ bool Parser::check_sintax(std::ifstream &source_file)
                         for(auto it = symbol_table.begin(); it != symbol_table.end(); it++)
                             if (it->first == operand_token[0])
                             {
-                                operand_token[0] = it->second;
+                                //operand_token[0] = it->second;
                                 found = true;
                             }
                         if (!found)
@@ -200,65 +300,37 @@ bool Parser::check_sintax(std::ifstream &source_file)
     std::cout << line_count << " " << instruction_table.size() << std::endl;
     source_file.close();
     line_count = 0;
+
+	// Verifica se todos os operadores foram corretamente mapeados
+	for (auto it = this->symbol_table.begin(); it != this->symbol_table.end(); it++)
+	{
+		if (it->second == -1)
+		{
+			auto error = "Operator '" + it->first + "' is unkown.";
+			throw std::runtime_error(error);
+		}
+	}
     
     // Segunda passada
+	std::string op_bin = "";
+
     for(auto it_symbol = instruction_table.begin(); it_symbol != instruction_table.end(); it_symbol++)
     {
-        
+		this->binary_code += this->get_instruction_opcode(it_symbol->first);
+		this->binary_code += this->get_instruction_opcode(it_symbol->second.first);
+		this->binary_code += this->get_instruction_opcode(it_symbol->second.second.first);
     }
-    
+	std::cout << this->binary_code << std::endl;
+        std::cout << this->binary_code.size() << std::endl;
     std::cout << "Done" << std::endl;
     
     return true;
 }
 
-std::vector<std::string> Parser::split_line(const std::string &text, char sep) {
-    std::vector<std::string> tokens;
-    std::size_t start = 0, end = 0;
-    while ((end = text.find(sep, start)) != std::string::npos) {
-        std::string temp = text.substr(start, end - start);
-        if (temp != "") tokens.push_back(temp);
-        start = end + 1;
-    }
-    std::string temp = text.substr(start);
-    if (temp != "") tokens.push_back(temp);
-    return tokens;
-}
-
-void Parser::load_instructions()
+std::string Parser::make_object(std::ifstream &source_file)
 {
-    std::ifstream symbols_file;
-    symbols_file.open("../data/symbols.d");
-    
-    if (!symbols_file.is_open())
-        throw std::runtime_error("Failed to open symbols file.");
-    
-    std::cout << "Loading symbols... ";
-    std::string line;
-    for (std::getline(symbols_file, line); !symbols_file.eof(); std::getline(symbols_file, line))
-    {
-        auto line_tokens = split_line(line, ' ');
-        this->instructions.push_back(std::make_pair(line_tokens[0], std::make_pair(std::stoi(line_tokens[1]), std::stoi(line_tokens[2]))));
-    }
-    symbols_file.close();
-    std::cout << "Done."  << std::endl;    
-}
-
-void Parser::load_operators()
-{
-    std::ifstream op_file;
-    op_file.open("../data/operator.d");
-    
-    if (!op_file.is_open())
-        throw std::runtime_error("Failed to open operator file.");
-    
-    std::cout << "Loading operators... ";
-    std::string line;
-    for(std::getline(op_file, line); !op_file.eof(); std::getline(op_file, line))
-    {
-        auto line_tokens = split_line(line, ' ');
-        this->operators.push_back(std::make_pair(line_tokens[0], std::stoi(line_tokens[1])));
-    }
-    op_file.close();
-    std::cout << "Done." << std::endl;
+	std::cout << std::endl << "Checking for sintax errors:" << std::endl;
+	check_sintax(source_file);
+	std::cout << "Ok" << std::endl;
+	return "";
 }
--- parser/parser.h
+++ parser/parser.h
@@ -17,36 +17,42 @@
 
 #include <iostream>
 
+using vecPair_StrInt = std::vector<std::pair<std::string, int>>;
+using vecPair_StrStr = std::vector<std::pair<std::string, std::string>>;
+using pair_StrPair_IntInt = std::pair<std::string, std::pair<int, int>>;
+using pair_StrPair_StrInt = std::pair<std::string, std::pair<std::string, int>>;
+
 class Parser {
 private:
     static Parser *instancia;
     // Vetor de instrucoes existentes <Nome_Instr <OpCode, Num_Operadores>>
-    std::vector<std::pair<std::string, std::pair<int, int>>> instructions;
+    std::vector<pair_StrPair_StrInt> instructions;
     // Vetor de registradores <Nome_regist, OpCode>
-    std::vector<std::pair<std::string, int>> operators;
+    vecPair_StrStr operators;
     // Vetor de instrucoes <OP, <R1, <R2, <Linha, Pos_Memoria>>>>
-    std::vector<std::pair<std::string, std::pair<std::string, std::pair<std::string, std::pair<int, int>>>>> instruction_table;
+    std::vector<std::pair<std::string, std::pair<std::string, pair_StrPair_IntInt>>> instruction_table;
     // Vetor de constantes <Nome, Valor/Endereco>
-    std::vector<std::pair<std::string, int>> symbol_table;
+    vecPair_StrInt symbol_table;
     // Vetor de OpCodes
-    std::vector<int> opcode_table;
+    std::string binary_code;
     
-    Parser();
-    Parser(const Parser& orig);
-    
+    Parser();    
     
     void load_instructions();
     void load_operators();
     
     std::vector<std::string> split_line(const std::string &text, char sep);
-    unsigned long dec_to_bin(int);
+    std::string dec_to_bin(int);
+	std::string get_instruction_opcode(std::string op);
+	std::string get_symbol_opcode(std::string op);
     
-    bool check_sintax(std::ifstream &source_file);
     bool is_operator(std::string op);
     bool is_valid_symbol(std::string si, int num_op);
     
     void add_instruction(std::string si, std::string op1, std::string op2, int line);
     void add_symbol(std::string si, int val);
+
+	bool check_sintax(std::ifstream &source_file);
     
 public:
     static Parser *get_instancia();
