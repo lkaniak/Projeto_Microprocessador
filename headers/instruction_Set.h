7a040cb1526a83726be9ea7b00dc2ab8d6f52577
Rafael Teixeira <rafael.szt@gmail.com>
2016-10-24 23:10:14 -0200

    + Arrumado algumas modificações indesejadas devido ao merge anterior

--- headers/instruction_Set.h
+++ headers/instruction_Set.h
@@ -17,6 +17,9 @@
 #include "instruction_line.h"
 #include "Instruction.h"
 #include "Operator.h"
+#include "REGISTER.h"
+#include "ALU.h"
+#include "BUS.h"
 
 class instruction_Set
 {
@@ -32,37 +35,37 @@ private:
     void load_operators();
         
     // Dois operadores
-    void mov(Operator r1, Operator r2); // MOV R1, R2
-    void mov(Operator r1, int num); //MOV R1, CONST
-    void add(Operator r1, Operator r2);
-    void sub(Operator r1, Operator r2);
-    void div(Operator r1, Operator r2);
-    void mul(Operator r1, Operator r2);
+    void mov(REGISTER * r1, REGISTER * r2); // MOV R1, R2
+    void mov(REGISTER * r1, int num); //MOV R1, CONST
+    void add(REGISTER * r1, REGISTER * r2);
+    void sub(REGISTER * r1, REGISTER * r2);
+    void div(REGISTER * r1, REGISTER * r2);
+    void mul(REGISTER * r1, REGISTER * r2);
     
     // Um operador
-    void cmp(Operator r1);
-    void _and(Operator r1);
-    void _or(Operator r1);
-    void _xor(Operator r1);
-    void push(Operator r1);
-    void pop(Operator r1);
-    void _not(Operator r1);
-    void inc(Operator r1);
-    void dec(Operator r1);
-    void load(Operator r1);
-    void store(Operator r1);
-    void interrupt(Operator r1);
-    void jmp(Operator r1);
-    void jg(Operator r1);
-    void je(Operator r1);
-    void jne(Operator r1);
-    void jl(Operator r1);
-    void jz(Operator r1);
-    void jnz(Operator r1);
-    void ja(Operator r1);
-    void jb(Operator r1);
-    void ret(Operator r1);
-    void call(Operator r1);
+    void cmp(REGISTER * r1);
+    void _and(REGISTER * r1);
+    void _or(REGISTER * r1);
+    void _xor(REGISTER * r1);
+    void push(REGISTER * r1);
+    void pop(REGISTER * r1);
+    void _not(REGISTER * r1);
+    void inc(REGISTER * r1);
+    void dec(REGISTER * r1);
+    void load(REGISTER * r1);
+    void store(REGISTER * r1);
+    void interrupt(REGISTER * r1);
+    void jmp(REGISTER * r1);
+    void jg(REGISTER * r1);
+    void je(REGISTER * r1);
+    void jne(REGISTER * r1);
+    void jl(REGISTER * r1);
+    void jz(REGISTER * r1);
+    void jnz(REGISTER * r1);
+    void ja(REGISTER * r1);
+    void jb(REGISTER * r1);
+    void ret(REGISTER * r1);
+    void call(REGISTER * r1);
 
     // Nenhum operador
     void start();
@@ -73,9 +76,12 @@ public:
     instruction_Set();
     ~instruction_Set();
 
-    void execute_instruction(instruction_line op);
+	void execute_instruction(std::string op, REGISTER * r1, REGISTER * r2);
+	void execute_instruction(std::string op, REGISTER * r1);
+	void execute_instruction(std::string op);
     //void execute_instruction(instruction_line op);
     Instruction *get_operation_info(const std::string name, int num_operators);
+	Instruction *get_operation_name(const std::string opcode);
 };
 
 #endif /* INSTRUCTION_SET_H */
